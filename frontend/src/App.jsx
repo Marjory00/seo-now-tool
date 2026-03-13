0e6894806fb950a36e67aa74d253418ef40d5c8d
Marjory D. Marquez <samaterra@hotmail.com>
2026-03-13 03:09:56 -0400

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import ResultsPage from './pages/ResultsPage'
 import Navbar from './components/Navbar'
+import './index.css'
 
 export default function App() {
   return (
